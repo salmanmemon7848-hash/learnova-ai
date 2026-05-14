4bac53ffb850973ab40c5179956819b8592d95ef
salmanmemon7848-hash <salmanmemon7848@gmail.com>
2026-05-14 23:19:31 +0530

    Update ESLint config as per new requirements

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,22 @@
-import nextVitals from "eslint-config-next/core-web-vitals.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
-  ...nextVitals,
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     rules: {
       "@typescript-eslint/no-unused-vars": "off",
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-empty-object-type": "off",
       "react/no-unescaped-entities": "off",
-      "react-hooks/set-state-in-effect": "off",
       "prefer-const": "off",
       "@next/next/no-img-element": "off",
     },
